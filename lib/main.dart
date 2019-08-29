ace8c00ac4b5dc4be7daedb766607f3c27df4d85
Jeferson Tomas <jeferson.tomas.santos@gmail.com>
2019-08-29 17:52:23 -0300

    Exemplo 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 /*void main() {
   runApp(
@@ -21,7 +22,7 @@ import 'package:flutter/material.dart';
   );
 }*/
 
-void main() {
+/*void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
@@ -39,4 +40,59 @@ void main() {
       ),
     )
   );
+}*/
+
+class MyAppBar extends StatelessWidget{
+  MyAppBar({this.title});
+  final Widget title;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.0,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null,
+          ),
+          Expanded(
+            child: title,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Material(
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+            title: Text(
+              'Example Title'
+              style: Theme.of(context).primaryTextTheme.title,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    title: 'My App',
+    home: MyScaffold(),
+    ));
 }
